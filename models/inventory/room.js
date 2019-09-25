12674c7c3fda7e478390bcd31ae454b8fe6ca6e9
luis.acevedo.ramirez <luis.acevedo.ramirez@accenture.com>
2019-09-25 12:14:29 -0300

    room types

--- models/inventory/room.js
+++ models/inventory/room.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const RoomSchema = Schema({
   name: { type: String, required: true },
   company: { type: Schema.ObjectId, ref: "Company", required: true },
-  type: { type: String, default: null },
+  type: { type: String, required: true },
   description: { type: String, default: null },
   createdAt: { type: String, default: null },
   updatedAt: { type: String, default: null },
